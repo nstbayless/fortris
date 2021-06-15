3128e5078deac7b8ff7452f41b2d053c5af7bd7e
nstbayless@gmail.com <nstbayless@gmail.com>
2021-06-15 11:46:39 -0700

    fortris gh page

--- build-js.sh
+++ build-js.sh
@@ -5,3 +5,14 @@ rm -r jsbuild/
 zip -9 -r fortris.love assets/ resources/ src/ jumper/ bitop/ main.lua conf.lua
 love.js -t fortris fortris.love jsbuild/
 
+if [ $1 == "--publish" ]
+then
+  if [ -d docs/ ]
+  then
+    rm -r docs/
+  fi
+  mkdir docs/
+  cp -r jsbuild/* docs/
+  rm docs/theme/bg.png
+  echo "Now commit and push on main branch to publish to github pages."
+fi
